9af1b2901bb20b807d67047b6d42621d61a65104
tristanfirdaus3 <tristangaming877@gmail.com>
2024-11-10 09:06:58 +0800

    hapusKeranjang.php

--- craftify_world-HUT/user/hapusKeranjang.php
+++ craftify_world-HUT/user/hapusKeranjang.php
@@ -2,19 +2,19 @@
     include("koneksi.php");
     session_start();
     $id_produk = $_GET['id_produk'];
-    $jumlah_barang = $_GET['jumlah_barang'];
+    $jumlah_Poin = $_GET['jumlah_Poin'];
 
     $query = $db->prepare("select * from produk where id=?");
     $query->bindParam(1, $id_produk);;
     $query->execute();
     $dataProduk = $query->fetch(); 
-    $resistanceBaru = $dataProduk['resistance'] + $jumlah_barang;
+    $resistanceBaru = $dataProduk['resistance'] + $jumlah_Poin;
     if($resistanceBaru <= 0){
         $resistanceBaru = 0;
     }
     
     if(!isset($_SESSION['id'])){
-        header("Location: ../index.php");
+        header("Location:index.php");
         exit;
     }
 
@@ -28,7 +28,7 @@
         $query->bindParam(1, $id_produk);
         $query->execute();
         $data = $query->fetchAll(); 
-        echo "<script> alert('Produk berhasil diHapus dari keranjang!!');
+        echo "<script> alert('Karya berhasil diHapus dari keranjang!!');
         window.location.replace('keranjang.php');</script>"; 
         die();
     }catch (PDOException $exception) {
